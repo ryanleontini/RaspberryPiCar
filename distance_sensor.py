2ea1cfcaa29fe6e84ba6b2e4c5d9170df56ae8c7
Ian Osborn <141860647+lzrd-wzrd@users.noreply.github.com>
2025-04-29 22:55:25 -0600

    Update distance_sensor.py

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,1 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.board)
+
+TRIGGER = 20
+ECHO = 21
+
+GPIO.setup(TRIGGER, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+GPIO.output(TRIGGER, False)
+
+
 
